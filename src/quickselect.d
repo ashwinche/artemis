61632fb4828fe9da7df5c562bb3719b2b78e494c
Ashwin <ashwinchetty@gmail.com>
2021-02-10 19:41:29 -0600

    port

--- src/quickselect.d
+++
@@ -1,3 +0,0 @@
-/home/ashwin/repo/artemis/build/../src/quickselect.o: \
- /home/ashwin/repo/artemis/build/../src/quickselect.cpp \
- /home/ashwin/repo/artemis/build/../include/quickselect.h
--- src/tgmm.d
+++
@@ -1,2 +0,0 @@
-/home/ashwin/repo/artemis/build/../src/tgmm.o: \
- /home/ashwin/repo/artemis/build/../src/tgmm.cpp
